8ccf8ecebbb77cf628ad1c27fc75849ff6fd194b
AmanRoy <72335987+amanroy2001@users.noreply.github.com>
2021-06-19 19:23:51 +0530

    Add files via upload

---
+++ stringtolowercase.java
@@ -0,0 +1,14 @@
+package codewithaman;
+import java.util.Scanner;
+public class stringtolowercase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		System.out.println("plase enter a string");
+		String s=sc.next();
+		System.out.println("your lower case string is "+s.toLowerCase());
+
+	}
+
+}
